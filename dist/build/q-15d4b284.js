28ecca280295869f0dfc933fc1d3fa2b5bbbda77
petval <petval@users.noreply.github.com>
2023-11-20 10:40:35 +0100

    adds /dist

---
+++ dist/build/q-15d4b284.js
@@ -0,0 +1,1 @@
+const e={text:"Menu",items:[{text:"Pages",items:[{text:"Services",href:"#"},{text:"Porfolio",href:"#"},{text:"About us",href:"#"},{text:"Terms",href:"/terms/"},{text:"Privacy",href:"/privacy/"}]},{text:"Blog",href:"/blog/"}]};export{e as default};
